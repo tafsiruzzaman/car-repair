e15b6787f399e66a5ed85e6a069a07c9da0c05b1
tafsiruzzaman <tafsirjaman100@gmail.com>
2021-10-17 14:56:11 +0600

    dynamic route for services complete

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Service.css';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Service = ({ service }) => {
     // const {service} = props;
-    const { name, price, description, img } = service;
+    const { id, name, price, description, img } = service;
     return (
         <div className="service">
             <img src={img} alt="" />
             <h3>{name}</h3>
             <h5>Price: {price}</h5>
             <p className="px-3">{description}</p>
+            <Link to={`/booking/${id}`}><Button variant="warning mb-4">Book {name.toLowerCase()}</Button></Link>
         </div>
     );
 };
